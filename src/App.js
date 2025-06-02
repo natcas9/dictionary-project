6f6632e825ff85c8083dceaa57e4d87c2b2ea3e1
natcas9 <natalia.casarin02@gmail.com>
2025-06-02 14:15:45 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
