64353733670f6a025f44428e118da959aa9d8747
natcas9 <natalia.casarin02@gmail.com>
2025-06-16 02:29:07 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,26 +1,22 @@
-import React, { useState } from "react"; // CORREGIDO
+import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState("");
-  const [results, setResults] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log("Full API response:", response); // DEBUG
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=7cfac3t6d90fo31f4fb0ff0151e8560c`;
-    axios
-      .get(apiUrl)
-      .then(handleResponse)
-      .catch((error) => {
-        console.error("API error:", error);
-      });
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
