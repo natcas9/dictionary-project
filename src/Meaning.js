90aecf790afd433415119fece79a9a926e1d10d7
natcas9 <natalia.casarin02@gmail.com>
2025-08-25 17:01:38 +0200

    fixed bugs

--- src/Meaning.js
+++ src/Meaning.js
@@ -14,11 +14,11 @@ export default function Meaning(props) {
               <br />
               <strong>Example:</strong>
               <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
