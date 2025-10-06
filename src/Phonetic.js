3f06f34a334f6337fd974efc38fdc56d52524699
natcas9 <natalia.casarin02@gmail.com>
2025-10-06 20:31:21 +0200

    phonetics integración

---
+++ src/Phonetic.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  console.log(props.phoenetic);
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetic.audio} target="_blank">
+        Listen
+      </a>
+      <br />
+      {props.phoenetic.text}
+    </div>
+  );
+}
