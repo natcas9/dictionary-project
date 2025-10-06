ccf42fc72c90144b444cf854123046464b1b337c
natcas9 <natalia.casarin02@gmail.com>
2025-10-06 21:39:05 +0200

    css update

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,9 +11,13 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
+  function search(keyword) {}
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   function search(event) {
     event.preventDefault();
-
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -26,9 +30,10 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <section>
-        <form onSubmit={search}>
+        <form onSubmit={handleSubmit}>
           <input type="search" onChange={handleKeywordChange} />
         </form>
+        <div className="hint">suggested words: sunset, wine, yoga</div>
       </section>
       <Results results={results} />
     </div>
