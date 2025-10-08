93dfb0169a169bca7c6807d55abf26d1a74eb9d2
natcas9 <natalia.casarin02@gmail.com>
2025-10-08 03:05:10 +0200

    photo integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    console.log(response.data);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "NSDZOqJEXteTFkog0rZI4H3U06dqOBVnvuAZexCKoBzz8H7iRcPnjjpj";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -36,6 +52,7 @@ export default function Dictionary(props) {
     return (
       <div className="Dictionary">
         <section>
+          <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
@@ -46,6 +63,7 @@ export default function Dictionary(props) {
           <div className="hint">suggested words: sunset, wine, yoga</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        {props.photos.map(function (photo, index) {
+          return <img src={photo.src.landscape} />;
+        })}
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
