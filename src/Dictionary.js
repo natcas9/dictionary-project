f029530f4783e077b1b9f7f8250b93ab715ac48a
natcas9 <natalia.casarin02@gmail.com>
2025-10-08 15:33:19 +0200

    photo gallery

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,7 +25,7 @@ export default function Dictionary(props) {
 
     let pexelsApiKey =
       "NSDZOqJEXteTFkog0rZI4H3U06dqOBVnvuAZexCKoBzz8H7iRcPnjjpj";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
     axios
       .get(pexelsApiUrl, {
