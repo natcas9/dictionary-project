bd2f3bd4c70b1b6de88dca0d0452fee35bd2cfc8
natcas9 <natalia.casarin02@gmail.com>
2025-10-08 15:38:45 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -5,17 +5,18 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div className="row"></div>
-        {props.photos.map(function (photo, index) {
-          console.log(photo);
-          return (
-            <div className="col-4" key={index}>
-              <a href={photo.src.original} target="_blank" rel="noreferrer">
-                <img src={photo.src.landscape} className="img-fluid" />
-              </a>
-            </div>
-          );
-        })}
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            console.log(photo);
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {
