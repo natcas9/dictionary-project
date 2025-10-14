ea00f15e0fa749d56c5e260a67e69f4c5c44e24b
natcas9 <natalia.casarin02@gmail.com>
2025-10-14 16:32:51 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "NSDZOqJEXteTFkog0rZI4H3U06dqOBVnvuAZexCKoBzz8H7iRcPnjjpj";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorization: `${pexelsApiKey}` };
     axios
       .get(pexelsApiUrl, {
         headers: headers,
